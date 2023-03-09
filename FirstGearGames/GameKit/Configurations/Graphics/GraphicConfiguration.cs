f9c658f53d13cd33a3002b925a01c59a8bd6e2b6
FirstGearGames <firstgeargames@gmail.com>
2023-03-08 19:58:51 -0500

    Backup.

---
+++ FirstGearGames/GameKit/Configurations/Graphics/GraphicConfiguration.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace GameKit.Configurations.Inventories
+{
+
+    public class GraphicsConfiguration : ScriptableObject
+    {
+        public Sprite Frame;
+        public Sprite BagHeader;
+        public Sprite BagEntry;
+        public Sprite SearchBar;
+        public Sprite Scrollbar;
+    }
+}
